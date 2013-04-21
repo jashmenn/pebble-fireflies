d4edd4120e5115b1f677d9c6b66c0acddd7abb25
Nate Murray <nate@natemurray.com>
2013-04-21 15:53:44 -0700

    basic particles, but they're not very random

--- src/pebble-fireflies.c
+++ src/pebble-fireflies.c
@@ -13,17 +13,19 @@ PBL_APP_INFO(MY_UUID,
              1, 0, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
-#define NUM_PARTICLES 1
+#define NUM_PARTICLES 100 
 #define COOKIE_MY_TIMER 1
 
 // typedefs
 typedef struct FParticle 
 {
   GPoint position;
-  GPoint gravCenter;
+  GPoint grav_center;
+  float dx;
+  float dy;
   float power;
 } FParticle;
-#define FParticle(px, py, gx, gy, power) ((FParticle){{(px), (py)}, {(gx), (gy)}, power})
+#define FParticle(px, py, gx, gy, power) ((FParticle){{(px), (py)}, {(gx), (gy)}, 0.0F, 0.0F, power})
 
 // globals
 FParticle particles[NUM_PARTICLES];
@@ -44,6 +46,33 @@ const GPathInfo SQUARE_POINTS = {
 
 GPath square_path;
 
+#define MAX_SPEED      2
+#define MAX_SPEED_NEG -2
+
+void update_particle(int i) {
+  // gravitate towards goal
+  particles[i].dx += -(particles[i].position.x - particles[i].grav_center.x)/particles[i].power;
+  particles[i].dy += -(particles[i].position.y - particles[i].grav_center.y)/particles[i].power;
+
+  // damping
+  particles[i].dx *= 0.8F;
+  particles[i].dy *= 0.8F;
+
+  // snap to max
+  if(particles[i].dx > MAX_SPEED)     particles[i].dx = MAX_SPEED;
+  if(particles[i].dx < MAX_SPEED_NEG) particles[i].dx = MAX_SPEED_NEG;
+  if(particles[i].dy > MAX_SPEED)     particles[i].dy = MAX_SPEED;
+  if(particles[i].dy < MAX_SPEED_NEG) particles[i].dy = MAX_SPEED_NEG;
+
+  particles[i].position.x += particles[i].dx;
+  particles[i].position.y += particles[i].dy;
+}
+
+void draw_particle(GContext* ctx, int i) {
+  int size = 1;
+  graphics_fill_circle(ctx, particles[i].position, size);
+}
+
 void update_square_layer(Layer *me, GContext* ctx) {
   (void)me;
 
@@ -58,20 +87,18 @@ void update_square_layer(Layer *me, GContext* ctx) {
 
   // update debug text layer
   static char test_text[100];
-  int now = sin_lookup(angle * 32768 / 90);
-  int then = linearmap(now, -65536, 65536, 0, 10);
+  // int now = sin_lookup(angle * 32768 / 90);
+  // int then = linearmap(now, -65536, 65536, 0, 10);
 
   xsprintf( test_text, "rand: %d", rand_between(1,5));
   text_layer_set_text(&text_header_layer, test_text);
 
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_circle(ctx, GPoint(window.layer.frame.size.w/2, window.layer.frame.size.h/2), then);
-}
-
-void update_particle(int i) {
-}
-
-void draw_particle(int i) {
+  // graphics_fill_circle(ctx, GPoint(window.layer.frame.size.w/2, window.layer.frame.size.h/2), then);
+  for(int i=0;i<NUM_PARTICLES;i++) {
+    update_particle(i);
+    draw_particle(ctx, i);
+  }
 }
 
 void handle_timer(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie) {
@@ -97,9 +124,18 @@ void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
   get_time(&current_time);
 }
 
+GPoint random_point_in_screen() {
+  return GPoint(rand_between(0, window.layer.frame.size.w+1), 
+                rand_between(0, window.layer.frame.size.h+1));
+}
+
 void init_particles() {
   for(int i=0; i<NUM_PARTICLES; i++) {
-    particles[i] = FParticle(0, 0, 0, 0, 0.0F);
+    GPoint start = random_point_in_screen();
+    GPoint goal = random_point_in_screen();
+    particles[i] = FParticle(start.x, start.y, 
+                             goal.x, goal.y, 
+                             1.0F);
   }
 }
 
