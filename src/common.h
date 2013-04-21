e75aff1a74d12eeef75eca086bdcde01dbff8c83
Nate Murray <nate@natemurray.com>
2013-04-21 15:29:18 -0700

    refactored helpers to common

---
+++ src/common.h
@@ -0,0 +1,3 @@
+unsigned lfsr_rand();
+long linearmap(long x, long in_min, long in_max, long out_min, long out_max);
+int rand_between(int low, int high);
--- src/pebble-fireflies.c
+++ src/pebble-fireflies.c
@@ -4,7 +4,7 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 #include "xprintf.h"
-#include "rand.h"
+#include "common.h"
 
 // defines
 #define MY_UUID { 0x74, 0x19, 0xF2, 0x5C, 0x82, 0x1B, 0x4B, 0xD8, 0x93, 0xD5, 0x98, 0x7E, 0x31, 0x15, 0xA8, 0xB6 }
@@ -23,6 +23,7 @@ typedef struct FParticle
   GPoint gravCenter;
   float power;
 } FParticle;
+#define FParticle(px, py, gx, gy, power) ((FParticle){{(px), (py)}, {(gx), (gy)}, power})
 
 // globals
 FParticle particles[NUM_PARTICLES];
@@ -43,15 +44,6 @@ const GPathInfo SQUARE_POINTS = {
 
 GPath square_path;
 
-//int linearmap(int value, int istart, int istop, int ostart, int ostop) {
-//  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
-//}
-
-long linearmap(long x, long in_min, long in_max, long out_min, long out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 void update_square_layer(Layer *me, GContext* ctx) {
   (void)me;
 
@@ -69,12 +61,17 @@ void update_square_layer(Layer *me, GContext* ctx) {
   int now = sin_lookup(angle * 32768 / 90);
   int then = linearmap(now, -65536, 65536, 0, 10);
 
-  xsprintf( test_text, "angle: %d", then);
+  xsprintf( test_text, "rand: %d", rand_between(1,5));
   text_layer_set_text(&text_header_layer, test_text);
 
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_circle(ctx, GPoint(window.layer.frame.size.w/2, window.layer.frame.size.h/2), then);
+}
 
+void update_particle(int i) {
+}
+
+void draw_particle(int i) {
 }
 
 void handle_timer(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie) {
@@ -87,7 +84,6 @@ void handle_timer(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie) {
   }
 }
 
-
 void layer_update_callback(Layer *me, GContext* ctx) {
   (void)me;
   (void)ctx;
@@ -99,6 +95,12 @@ void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
 
   PblTm current_time;
   get_time(&current_time);
+}
+
+void init_particles() {
+  for(int i=0; i<NUM_PARTICLES; i++) {
+    particles[i] = FParticle(0, 0, 0, 0, 0.0F);
+  }
 }
 
 void handle_init(AppContextRef ctx) {
@@ -115,6 +117,8 @@ void handle_init(AppContextRef ctx) {
   // layer.update_proc = &layer_update_callback;
   // layer_add_child(&window.layer, &layer);
 
+  init_particles();
+
   // setup debugging text layer
   text_layer_init(&text_header_layer, window.layer.frame);
   text_layer_set_text_color(&text_header_layer, GColorWhite);
@@ -123,7 +127,6 @@ void handle_init(AppContextRef ctx) {
   text_layer_set_font(&text_header_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
   layer_add_child(&window.layer, &text_header_layer.layer);
  
-
   layer_init(&square_layer, GRect(0,0, window.layer.frame.size.w, window.layer.frame.size.h));
   square_layer.update_proc = update_square_layer;
   layer_add_child(&window.layer, &square_layer);
